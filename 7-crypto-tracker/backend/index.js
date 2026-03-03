98550d2c057e358147ccb9c2e1661ed3442129f0
Mohammad Ahmed Khan <ahmedtariq1812@gmail.com>
2026-03-03 23:55:59 +0530

    postgresSQL

---
+++ 7-crypto-tracker/backend/index.js
@@ -0,0 +1,53 @@
+// index.js
+require('dotenv').config(); // Loads variables from .env
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 8082;
+
+// Middleware
+app.use(cors()); // Allow cross-origin requests from your frontend
+app.use(express.json()); // Parse JSON request bodies
+
+app.get('/health', (req,res) => {
+    res.status(200).json({
+        status: "OK",
+        message: "Server is running successfully"
+    });
+});
+
+//external api route 
+app.get('/api/crypto', async (req, res) => {
+  try{
+    const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false', {
+        params: {
+            vs_currency: 'usd',
+            order: 'market_cap_desc',
+            per_page: 10,
+            page: 1,
+            sparkline: false
+        },
+    });
+    res.status(200).json(response.data); 
+  } catch (error){
+    console.log("Error fetching data from CoinGecko:", error.message);
+    res.status(500).json({ error: "Failed to fetch crypto data" });
+  }
+});
+
+app.get('/api/user', (req,res) => {
+    const user = {
+        id: 101,
+        name: "Ahmed Khan",
+        email: "[EMAIL_ADDRESS]",
+        loggedIn: true
+    };
+    res.status(200).json(user);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
